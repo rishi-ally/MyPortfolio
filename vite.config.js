93e7c5985dc3ab0470d0a453863ec20f0e8dbe48
Rishikesh <rishikeshpurohit94@gmail.com>
2024-10-26 10:57:51 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: 'https://github.com/rishi-ally/MyPortfolio-website', // Adjust this line
-});
+// vite.config.js
+export default {
+  base: '/MyPortfolio-website/',
+  // other configurations
+}
